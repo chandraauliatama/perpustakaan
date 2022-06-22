1531c85a17cbfa761230d8ec77692d3e7c14cbc2
chandraauliatama <chandraaulia4@gmail.com>
2022-06-22 18:15:20 +0700

    crud manageuser working

--- app/Http/Controllers/Admin/ManageUserController.php
+++ app/Http/Controllers/Admin/ManageUserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class ManageUserController extends Controller
 {
@@ -15,7 +18,7 @@ class ManageUserController extends Controller
      */
     public function index()
     {
-        $users = User::paginate(10);
+        $users = User::orderBy('role_id')->paginate(10);
         return view('admin.ManageUser.index', compact('users'));
     }
 
@@ -26,7 +29,8 @@ class ManageUserController extends Controller
      */
     public function create()
     {
-        //
+        $roles = Role::all();
+        return view('admin.ManageUser.create', compact('roles'));
     }
 
     /**
@@ -37,7 +41,14 @@ class ManageUserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = $request->validate([
+        'name' => 'required|string|min:3|max:255',
+        'email' => "required|email|unique:users",
+        'role_id' => 'required|in:1,2,3,4',
+        'password' => ['required', 'confirmed', Password::defaults()]
+        ]);
+        User::create($user);
+        return redirect()->route('admin.user.index');
     }
 
     /**
@@ -59,7 +70,9 @@ class ManageUserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+        $roles = Role::all();
+        return view('admin.ManageUser.edit', compact('user', 'roles'));
     }
 
     /**
@@ -71,7 +84,18 @@ class ManageUserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+        'name' => 'required|string|min:3|max:255',
+        'email' => "required|email|unique:users,email,{$id}",
+        'role_id' => 'required|in:1,2,3,4'
+        ]);
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role_id = $request->role_id;
+        !$request->has('reset-password') ?: $user->password = Hash::make('12345678');
+        $user->save();
+        return redirect()->route('admin.user.index');
     }
 
     /**
@@ -82,6 +106,8 @@ class ManageUserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->route('admin.user.index');
     }
 }
---
+++ resources/views/admin/ManageUser/edit.blade.php
@@ -0,0 +1,78 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Data User') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form method="POST" action="{{ route('admin.user.update', $user->id) }}">
+                        @csrf
+                        @method('PUT')
+
+                        @if ($errors->any())
+                            <div class="text-red-500">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <!-- Nama Lengkap -->
+                        <div>
+                            <x-label for="name" :value="__('Nama')" />
+
+                            <x-input id="name" class="block mt-1 w-full" type="text" name="name"
+                                :value="$user->name" required autofocus />
+                        </div>
+
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-label for="email" :value="__('Email')" />
+
+                            <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                :value="$user->email" required autofocus />
+                        </div>
+
+                        <!-- Roles -->
+                        <div class="mt-4">
+                            <x-label for="role_id" :value="__('Role')" />
+                            <select name="role_id" id="role_id"
+                                class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                <option>Pilih Role</option>
+                                @foreach ($roles as $role)
+                                    <option value="{{ $role->id }}"
+                                        {{ $user->role_id == $role->id ? 'selected' : '' }}>{{ $role->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <!-- Password -->
+                        <div class="mt-4">
+                            <label for="reset-password" class="inline-flex relative items-center cursor-pointer">
+                                <input type="checkbox" id="reset-password" name="reset-password" class="sr-only peer">
+                                <div
+                                    class="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                                </div>
+                                <span class="ml-3 text-sm font-medium text-gray-700 dark:text-gray-300">Reset
+                                    Password</span>
+                            </label>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-button class="ml-3">
+                                {{ __('Ubah Data') }}
+                            </x-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
